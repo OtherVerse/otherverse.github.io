cb447826e25435e76eace09a24a08129f96f2c5c
unknown <you@example.com>
2024-01-12 16:46:03 +0300

    fix header

--- assets/js/tg.js
+++ assets/js/tg.js
@@ -4,15 +4,9 @@ tg.expand(); // Убедитесь, что это допустимо в ваше
 
 document.addEventListener("DOMContentLoaded", function() {
     if (tg && tg.initDataUnsafe) {
-        let user_name = document.getElementById("first_name");
         let userAvatar = document.getElementById("userAvatar");
 
         if (tg.initDataUnsafe.user) {
-            // Установка имени пользователя
-            let p = document.createElement("p");
-            p.innerText = tg.initDataUnsafe.user.first_name;
-            user_name.appendChild(p);
-
             // Установка аватара пользователя или вашей собственной картинки
             userAvatar.src = tg.initDataUnsafe.user.photo_url ? tg.initDataUnsafe.user.photo_url : 'assets/img/sample/avatar/avatar1.jpg';
         }
